595b15da8aa10a439ac715981d6fe00a4ecda052
Arkadiusz Jadach <arek667@gmail.com>
2019-07-12 19:11:00 +0200

    Dodane metody

--- HelloWorld1/Program.cs
+++ HelloWorld1/Program.cs
@@ -6,13 +6,81 @@ namespace HelloWorld1
     {
         static void Main(string[] args)
         {
-            string name = args[0];
+            ValueTypes();
+            for (; ; )
+            {
+                string name = Greetings();
+
+                Age();
+                Settings();
+            }
+
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("minLong = " + minLong);
+        }
+
+
+        ///<summary>
+        /// Program settings - font color etc
+        /// </summary>
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        ///<summary>
+        /// check the user's age and tell him what he can buy
+        /// </summary>
+        private static void Age()
+        {
+            Console.Write("Wpisz ile masz lat: ");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+
+            Console.WriteLine("Masz " + age + " lat(a).");
+
+            if (age > 17)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Jesteś pełnoletni. Możesz kupić alkohol.");
+            }
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Wprowadziłeś niewłaściwy wiek :( ");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Możemy Ci zaproponować mleko.");
+            }
+        }
+
+        ///<summary>
+        /// program says hello to the user
+        /// </summary>
+        private static string Greetings()
+        {
+            Console.Write("Podaj imię: ");
+
+            string name = Console.ReadLine();
             Console.WriteLine("Witaj " + name);
 
-            //foreach (var name in args)
-            //{
-            //    Console.WriteLine("Witaj " + name);
-            //}
+            return name;
         }
     }
 }
