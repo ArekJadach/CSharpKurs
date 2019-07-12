fd6f88060d603d8f8381c21ad36ec745538ce495
Arkadiusz Jadach <arek667@gmail.com>
2019-07-13 01:10:32 +0200

    StudentDiary added

---
+++ StudentDiary/Diary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StudentDiary
+{
+    class Diary
+    {
+        //Stan (zmienne - pola)
+        List<float> ratings = new List<float>();
+        
+
+        //Zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        //Oblicza średnią z ocen
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+        }
+
+        //Zwraca najwyższą ocenę
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        //Zwraca najniższą ocenę
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+           
+            for (; ; )
+            {
+                Console.WriteLine("Podaj ocenę z zakresu 1 - 10 : ");
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
+
+                if (rating == 0)
+                {
+                    break;
+                }
+                if (result)
+                {
+                    if (rating > 0 && rating <=10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna liczba. Podaj wartość z zakresu 1 - 10");
+                    }
+                }
+                
+            }
+
+            Console.WriteLine("Średnia twoich ocen to: " + diary.CalculateAverage());
+            Console.WriteLine("Najwyższa ocena to: " + diary.GiveMaxRating());
+            Console.WriteLine("Najniższa ocena to: " + diary.GiveMinRating());
+            Console.ReadKey();
+        }   
+    }
+}
